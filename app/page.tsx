3dc5a72f99b800c2f47bad49092d33d3ca57cd3e
mifouts <fouts.mitch15@gmail.com>
2023-07-31 16:41:49 -0400

    cleaning up css

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="text-6xl">
       <h1>Lets build a gpt 4 sports information app</h1>
     </main>
   )
