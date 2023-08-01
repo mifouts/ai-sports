179b27761832c0f19102a08aba37981985861eb7
mifouts <fouts.mitch15@gmail.com>
2023-07-31 22:32:24 -0400

    installing drop down menu for countries

--- components/CityPicker.tsx
+++ components/CityPicker.tsx
@@ -2,6 +2,16 @@
 
 import React from 'react'
 import { Country, City } from "country-state-city"
+import Select from 'react-select';
+
+const options = Country.getAllCountries().map(country => ({
+    value: {
+        latitude: country.latitude,
+        longitude: country.longitude,
+        isoCode: country.isoCode,
+    },
+    label: country.name,
+}))
 
 function CityPicker() {
   return (
