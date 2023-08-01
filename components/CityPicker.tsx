657892207dd2f953d0ccc7340877bc46a77ae755
mifouts <fouts.mitch15@gmail.com>
2023-07-31 22:39:52 -0400

    making city picker functional

--- components/CityPicker.tsx
+++ components/CityPicker.tsx
@@ -1,8 +1,28 @@
 "use client";
 
-import React from 'react'
+import React, { useState } from 'react'
 import { Country, City } from "country-state-city"
 import Select from 'react-select';
+
+type option = {
+    value: {
+        latitude: string;
+        longitude: string;
+        isoCode: string;
+    },
+    label: string;
+} | null;
+
+type cityOption = {
+    value: {
+        latitude: string;
+        longitude: string;
+        countryCode: string;
+        name: string;
+        stateCode: string;
+    },
+    label: string;
+} | null;
 
 const options = Country.getAllCountries().map(country => ({
     value: {
@@ -14,6 +34,8 @@ const options = Country.getAllCountries().map(country => ({
 }));
 
 function CityPicker() {
+    const [selectedCountry, setSelectedCountry] = useState<option>(null);
+    const [selectedCity, setSelectedCity] = useState<cityOption>(null);
   return (
     <div>
         <Select 
