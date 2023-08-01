f70f7038044a1ebc6f10c6ae27764824215882ae
mifouts <fouts.mitch15@gmail.com>
2023-07-31 23:21:13 -0400

    messing with gradient

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import CityPicker from "@/components/CityPicker";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-[#552586] to-[#6A359C] p-10 flex flex-col justify-center items-center">
+    <div className="min-h-screen bg-gradient-to-br from-[#B589D6] to-[#552586] p-10 flex flex-col justify-center items-center">
       <Card className="max-w-4xl mx-auto bg-white rounded-lg">
         <Text className="text-6xl font-bold text-center mb-10">Sports AI</Text>
         <Subtitle className="text-xl text-center">Powered by OpenAI, Next.js 13.3, Tailwind CSS, Tremor 2.0 + More!</Subtitle>
@@ -13,7 +13,7 @@ export default function Home() {
         <Divider className="my-10" />
 
 
-        <Card className="bg-gradient-to-br from-[#552586] to-[#6A359C] rounded-lg">
+        <Card className="bg-gradient-to-br from-[#B589D6] to-[#552586] rounded-lg">
           <CityPicker />
         </Card>
       </Card>
