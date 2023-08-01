8b2307447a20c1c8da84968f61e4a3808ac0b557
mifouts <fouts.mitch15@gmail.com>
2023-07-31 22:34:25 -0400

    adding flare

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <div className="min-h-screen bg-gradient-to-br from-[#552586] to-[#6A359C] p-10 flex flex-col justify-center items-center">
       <Card className="max-w-4xl mx-auto bg-white rounded-lg">
         <Text className="text-6xl font-bold text-center mb-10">Sports AI</Text>
-        <Subtitle className="text-xl text-center">Powered by OpenAI, Next.js 13.3, Tailwind CSS, Tremor 2.0 + More</Subtitle>
+        <Subtitle className="text-xl text-center">Powered by OpenAI, Next.js 13.3, Tailwind CSS, Tremor 2.0 + More!</Subtitle>
 
         <Divider className="my-10" />
 
