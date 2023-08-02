c85a98ec4e50db813a911f7a4ff064a319907a0e
mifouts <fouts.mitch15@gmail.com>
2023-08-02 17:37:53 -0400

    creating sports page

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
-function SportsPage() {
+type Props = {
+  params: {
+    city: string,
+    lat: string,
+    long: string,
+  }
+}
+
+function SportsPage({ params: { city, lat, long }}: Props) {
   return (
-    <div>Welcome to the sports page</div>
+    <div>Welcome to the sports page {city} {lat} {long}</div>
   )
 }
 
