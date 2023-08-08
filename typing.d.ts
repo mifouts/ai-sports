741e031ee87dc8fb9d14d36bc1403ac743ae3f17
mifouts <fouts.mitch15@gmail.com>
2023-08-08 16:55:00 -0400

    mapping queries

--- typing.d.ts
+++ typing.d.ts
@@ -5,3 +5,16 @@ interface CurrentWeather {
     winddirection: number;
     windspeed: number;
   }
+
+  interface DailyUnits {
+    apparent_temperature_max: string;
+  apparent_temperature_min: string;
+  sunrise: string;
+  sunset: string;
+  temperature_2m_max: string;
+  temperature_2m_min: string;
+  time: string;
+  uv_index_clear_sky_max: string;
+  uv_index_max: string;
+  weathercode: string;
+  }
