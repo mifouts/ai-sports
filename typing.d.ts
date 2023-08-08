377f412ab4d4cb1be8bf572fc112953e2ab08ecc
mifouts <fouts.mitch15@gmail.com>
2023-08-08 16:57:36 -0400

    cleaning up code

--- typing.d.ts
+++ typing.d.ts
@@ -8,13 +8,13 @@ interface CurrentWeather {
 
   interface DailyUnits {
     apparent_temperature_max: string;
-  apparent_temperature_min: string;
-  sunrise: string;
-  sunset: string;
-  temperature_2m_max: string;
-  temperature_2m_min: string;
-  time: string;
-  uv_index_clear_sky_max: string;
-  uv_index_max: string;
-  weathercode: string;
+    apparent_temperature_min: string;
+    sunrise: string;
+    sunset: string;
+    temperature_2m_max: string;
+    temperature_2m_min: string;
+    time: string;
+    uv_index_clear_sky_max: string;
+    uv_index_max: string;
+    weathercode: string;
   }
